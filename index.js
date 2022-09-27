123aed96447093899ad5a6dad3e2cd22cc9c9cce
chennu_pooja <20951a6632@iare.ac.in>
2022-09-27 10:16:40 +0530

    Added files

--- index.js
+++ index.js
@@ -1,16 +1,43 @@
-// const hello = () => {
-//     console.log("Hello Node.js!");
-// };
-
-// hello();
-
 const http = require("http");
 const fs = require("fs");
 
-const server = http.createServer((req,res) => {
-    fs.readFile("sample.txt",(err,data)=>{
-        res.end(data);
-    })
-})
+let homeContent = "";
+let projectContent = "";
+//let registrationContent = "";
 
-server.listen(3000);
+fs.readFile("home.html", (err, home) => {
+  if (err) {
+    throw err;
+  }
+  homeContent = home;
+});
+
+fs.readFile("project.html", (err, project) => {
+  if (err) {
+    throw err;
+  }
+  projectContent = project;
+});
+
+// fs.readFile("registration.html", (err, project) => {
+//   if (err) {
+//     throw err;
+//   }
+//   registrationContent = project;
+// });
+
+http.createServer((request, response) => {
+    let url = request.url;
+    response.writeHead(200, { "Content-Type": "text/html" });
+    switch (url) {
+      case "/project":
+        response.write(projectContent);
+        response.end();
+        break;
+      default:
+        response.write(homeContent);
+        response.end();
+        break;
+    }
+  })
+  .listen(3000);
