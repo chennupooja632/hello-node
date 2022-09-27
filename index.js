aad0468ed957cf8ba01cc605d8000a0d318b98ad
chennu_pooja <20951a6632@iare.ac.in>
2022-09-27 10:13:22 +0530

    Modified files

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-const hello = () => {
-    console.log("Hello Node.js!");
-};
+// const hello = () => {
+//     console.log("Hello Node.js!");
+// };
 
-hello();
+// hello();
+
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req,res) => {
+    fs.readFile("sample.txt",(err,data)=>{
+        res.end(data);
+    })
+})
+
+server.listen(3000);
